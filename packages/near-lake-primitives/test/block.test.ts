85808095c2fbc127771d232d020249291860aa03
Morgan Mccauley <morgan@mccauley.co.nz>
2023-11-14 08:46:01 +1300

    fix: Ensure event logs are parsed correctly

--- packages/near-lake-primitives/test/block.test.ts
+++ packages/near-lake-primitives/test/block.test.ts
@@ -3,7 +3,7 @@ import { readFile } from 'fs/promises';
 import { Block } from '../src/types';
 
 describe('Block', () => {
-  it('reads event logs', async () => {
+  it('parses event logs', async () => {
     let streamerMessageBuffer = await readFile(`${__dirname}/../../../blocks/61321189.json`);
     let streamerMessage = JSON.parse(streamerMessageBuffer.toString());
     let block = Block.fromStreamerMessage(streamerMessage);
